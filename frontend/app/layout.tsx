975eb4fac9db6955ed54515b6741c75ecbdc007e
Adithya Krishnan <adithyakrishnan@Adithyas-MacBook-Air.local>
2026-01-20 19:51:31 +0530

    fix: hardcode adsense script in head for crawler

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -27,16 +27,16 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1920333630540777"
+            crossOrigin="anonymous"
+          ></script>
+        </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <Script
-            id="adsense-init"
-            async
-            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_ID || "ca-pub-1920333630540777"}`}
-            crossOrigin="anonymous"
-            strategy="afterInteractive"
-          />
           {children}
         </body>
       </html>
